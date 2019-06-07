4f591ce86b9d6eb946e0a6034eff668c73949ee6
Jeff Berlin <jeff@jbrally.com>
2019-06-07 11:03:55 -0400

    Editing

--- public/js/angular.js
+++ public/js/angular.js
@@ -124,7 +124,7 @@ app.controller('resumeCtrl', function($scope, $document) {
 		// body inside the modal
 			modalBody: 'The event flyer for DJ Straftanz\'s \'A Midsummer Night\'s Dream\' show. This was created using Adobe Photoshop.',
 		// handles the link description in modal
-			modalViewType: 'Flyer'
+			modalViewType: 'Full-Size Flyer'
     },
     {
       title: 'May the Fourth Flyer',
@@ -142,7 +142,7 @@ app.controller('resumeCtrl', function($scope, $document) {
 		// body inside the modal
 			modalBody: 'The event flyer for DJ Straftanz\'s \'May the Fourth\' show. This was created using Adobe Photoshop.',
 		// handles the link description in modal
-			modalViewType: 'Flyer'
+			modalViewType: 'Full-Size Flyer'
     },
     {
       title: 'BMT Micro Catalog',
